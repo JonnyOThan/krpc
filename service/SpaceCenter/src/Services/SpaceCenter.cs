c70b3e670e4e8e5f1b3cf017c2e70b5734fb8dd5
Jon Cable <jonnyothan@gmail.com>
2019-07-02 21:15:34 -0700

    allow setting ActiveVessel from spacecenter

--- service/SpaceCenter/src/Services/SpaceCenter.cs
+++ service/SpaceCenter/src/Services/SpaceCenter.cs
@@ -73,13 +73,25 @@ namespace KRPC.SpaceCenter.Services
         /// <summary>
         /// The currently active vessel.
         /// </summary>
-        [KRPCProperty (GameScene = GameScene.Flight)]
+        [KRPCProperty]
         public static Vessel ActiveVessel {
             get { return new Vessel (FlightGlobals.ActiveVessel); }
             set {
                 if (ReferenceEquals (value, null))
                     throw new ArgumentNullException ("ActiveVessel");
-                FlightGlobals.ForceSetActiveVessel (value.InternalVessel);
+				if (ReferenceEquals(value.InternalVessel, null))
+					throw new ArgumentException("ActiveVessel.InternalVessel");
+				if (HighLogic.LoadedSceneIsFlight)
+				{
+					FlightGlobals.ForceSetActiveVessel(value.InternalVessel);
+				}
+				else
+				{
+					int vesselIndex = HighLogic.CurrentGame.flightState.protoVessels.FindIndex(pv => pv.vesselID == value.InternalVessel.id);
+					if (vesselIndex == -1)
+						throw new InvalidOperationException("No vessel found");
+					FlightDriver.StartAndFocusVessel(HighLogic.CurrentGame, vesselIndex);
+				}
                 throw new YieldException (new ParameterizedContinuationVoid<int> (WaitForVesselSwitch, 0));
             }
         }
